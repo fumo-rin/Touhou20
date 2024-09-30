4a30698eefbb869a81e374a6883f2ddac802369a
saunaquest <steelchairbusiness@gmail.com>
2024-09-30 15:24:15 +0200

    7

--- Assets/Bremsengine/Attack Handler/Handler Types/PlayerAttackHandler.cs
+++ Assets/Bremsengine/Attack Handler/Handler Types/PlayerAttackHandler.cs
@@ -11,6 +11,7 @@ namespace Bremsengine
         [SerializeField] float TESTINGattackDelay = 0.6f;
         [SerializeField] BaseAttack TESTINGcurrentAttack;
         [SerializeField] Vector2 overrideDirection;
+        bool attackHeld;
         Vector2 lastVelocity;
         Vector2 GetAttackDirection => overrideDirection == Vector2.zero ? (lastVelocity == Vector2.zero ? Vector2.right : lastVelocity) : overrideDirection;
         public override bool CanAttack()
@@ -23,21 +24,43 @@ namespace Bremsengine
             {
                 lastVelocity = movementRigidbody.linearVelocity;
             }
+            if (attackHeld)
+            {
+                if (CanAttack())
+                {
+                    nextAttackTime = Time.time + TESTINGattackDelay;
+                    TriggerAttack(TESTINGcurrentAttack);
+                }
+            }
         }
         private void Start()
         {
             PlayerInputController.actions.Player.Fire.performed += OnAttackInput;
+            PlayerInputController.actions.Player.Fire.canceled += OnAttackInput;
         }
         private void OnDestroy()
         {
             PlayerInputController.actions.Player.Fire.performed -= OnAttackInput;
+            PlayerInputController.actions.Player.Fire.canceled -= OnAttackInput;
         }
         private void OnAttackInput(InputAction.CallbackContext c)
         {
-            if (CanAttack())
+            switch (c.phase)
             {
-                nextAttackTime = Time.time + TESTINGattackDelay;
-                TriggerAttack(TESTINGcurrentAttack);
+                case InputActionPhase.Disabled:
+                    break;
+                case InputActionPhase.Waiting:
+                    break;
+                case InputActionPhase.Started:
+                    break;
+                case InputActionPhase.Performed:
+                    attackHeld = true;
+                    break;
+                case InputActionPhase.Canceled:
+                    attackHeld = false;
+                    break;
+                default:
+                    break;
             }
         }
         public override void TriggerAttack(BaseAttack attack)
